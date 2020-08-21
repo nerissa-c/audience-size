fb603d9de7e03469104331373872e0a6fa71c36d
nerissa-c <49270530+nerissa-c@users.noreply.github.com>
2020-08-21 09:58:01 +0100

    Update audience.py
    Generalised Path and Account info

--- audience.py
+++ audience.py
@@ -4,19 +4,19 @@ import pandas as pd
 from datetime import datetime
 
 try:
-    current_data = pd.read_csv("data/AU.csv")
+    current_data = pd.read_csv("data/test.csv")
     csv_exists = True
 except Exception:
     csv_exists = False
 
-path_to_creds = "/Users/nerissaculi/Documents/Clients/creds/amex_creds/amex.yaml"
+path_to_creds = "path_to_your_creds"
 client = google.ads.google_ads.client.GoogleAdsClient.load_from_storage(path=path_to_creds)
 status_enum = client.get_type('UserListAccessStatusEnum').UserListAccessStatus.Name
 google_ads_service = client.get_service('GoogleAdsService', version='v1')
 date = datetime.now().date()
 
 query = "SELECT user_list.size_for_search, user_list.size_for_display, user_list.name, user_list.id, user_list.account_user_list_status FROM user_list"
-account_id = "365-276-1258"
+account_id = "select_account"
 results = google_ads_service.search(str(account_id).replace("-", ""), query=query, page_size=500)
 rows = []
 for row in results:
@@ -31,9 +31,9 @@ df = pd.DataFrame(data = rows, columns = columns)
 print(df)
 
 if csv_exists:
-    pd.concat([current_data, df]).to_csv("data/AU.csv", index=False)
+    pd.concat([current_data, df]).to_csv("data/test.csv", index=False)
 else:
-    df.to_csv("data/AU.csv",index=False)
+    df.to_csv("data/test.csv",index=False)
 
 
 
